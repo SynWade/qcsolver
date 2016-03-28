0915139fbe23fac7b2ca9ffa320d60d50d822964
Goodness Onyedikachukwu Okwubido Williams <Gokwubidowilliams-cc@conestogac.on.ca>
2016-03-27 20:05:30 -0400

    time stamp metadata created but validation left to complete

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # qcsolver
 http://rogerdudler.github.io/git-guide/
+
+to make new branch
+make sure to do:
+git checkout master
+git fetch
+git pull origin master
+git checkout -b nameOfNewBranch
+
+to commit changes to branch
+close visual studio
+git status
+git add .
+git commit -m "message for commit"
+git push origin nameOfNewBranch
---
+++ qcsolver/qcsolver/Models/MetadataClass/TimeStampMetadata.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace qcsolver.Models
+{
+    [MetadataType(typeof(TimeStampMetadata))]
+    public partial class Timestamp : IValidatableObject
+    {
+
+        private onsightdbEntities db = new onsightdbEntities();
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (timestampId != null)
+            {
+                timestampId = timestampId;
+                yield return ValidationResult.Success;
+            }
+
+            if (timeIn != null)
+            {
+                timeIn = timeIn;
+                yield return ValidationResult.Success;
+            }
+
+            if (timeOut != null)
+            {
+                timeOut = timeOut;
+                yield return ValidationResult.Success;
+            }
+
+            if (person != null)
+            {
+                person = person;
+                yield return ValidationResult.Success;
+            }
+
+            if (constructionSite != null)
+            {
+                constructionSite = constructionSite;
+                yield return ValidationResult.Success;
+            }
+        }
+    }
+
+
+
+
+
+    public class TimeStampMetadata
+    {
+        [Display(Name= "Time StampId")]
+        public int timestampId { get; set; }
+
+        [Display(Name= "Time In")]
+        public System.DateTime timeIn { get; set; }
+
+        [Display(Name= "Time Out")]
+        public Nullable<System.DateTime> timeOut { get; set; }
+
+        [Display(Name= "Person")]
+        public int person { get; set; }
+
+        [Display(Name= "Construction Site")]
+        public int constructionSite { get; set; }
+    }
+}
